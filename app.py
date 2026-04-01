a0175849cface77053926b2b0c21e7bad84463d7
Rohit Kharat <kharatrohit1130@gmail.com>
2026-04-01 20:49:39 +0530

    Remove comment for main dashboard route

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
-# Route to serve the main dashboard
+
 @app.route("/")
 def index():
     return render_template("index.html")
